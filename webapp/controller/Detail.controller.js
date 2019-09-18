61e454899f210fa661919886a7e6275dd0483710
Luciana Lima <lucianalima.lml@gmail.com>
2019-09-18 14:11:25 -0300

    Consertando detail

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,37 +1,28 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "../model/formatter",
     "sap/ui/core/routing/History",
     "sap/m/MessageToast",
-    "sap/ui/core/UIComponent"
-], function(Controller, JSONModel, formatter, History, MessageToast, UIComponent) {
+    "sap/ui/model/json/JSONModel"
+], function(Controller, History, MessageToast, JSONModel) {
     "use strict";
     return Controller.extend("llima.sap.ui.demo.walkthrough.controller.Detail", {
-        formatter: formatter,
         onInit: function() {
-            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+
             var oViewModel = new JSONModel({
                 currency: "EUR"
             });
             this.getView().setModel(oViewModel, "view");
+
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
         },
         _onObjectMatched: function(oEvent) {
             this.byId("rating").reset();
             this.getView().bindElement({
-                path: decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
-                model: "invoice",
-                parameters: {
-                    expand: "shipper",
-                },
-            });
-            this.byId('invoiceListShipper').bindElement({
-                path: decodeURIComponent(oEvent.getParameter("arguments").invoicePath) + '/shipper',
-                model: "invoice",
+                path: "/" + oEvent.getParameter("arguments").invoicePath,
+                model: "invoice"
             });
         },
-
         onNavBack: function() {
             var oHistory = History.getInstance();
             var sPreviousHash = oHistory.getPreviousHash();
@@ -39,15 +30,13 @@ sap.ui.define([
             if (sPreviousHash !== undefined) {
                 window.history.go(-1);
             } else {
-                var oRouter = UIComponent.getRouterFor(this);
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.navTo("overview", {}, true);
             }
         },
-
         onRatingChange: function(oEvent) {
             var fValue = oEvent.getParameter("value");
             var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
-
             MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
         }
     });
